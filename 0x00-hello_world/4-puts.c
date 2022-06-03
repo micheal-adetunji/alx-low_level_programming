28b62a129fdc530814315a4ff00115e2a477b748
micheal <mikexpress24@gmail.com>
2022-06-03 22:17:23 +0000

    created 4-puts.c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 /**
- * main outputs line of string with puts function
+ * main -  outputs line of string
+ *
+ * Outputs line of string with puts function
+ * Return: returns with value 0
  */
 int main(void)
 {
