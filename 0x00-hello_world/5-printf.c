5cfdbd9f6299b8bb2a3a0d14f443aefc8c2084d1
micheal <mikexpress24@gmail.com>
2022-06-03 22:26:57 +0000

    created 5-printf.c program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - outputs line of text
+ *
+ * outputs line of text with printf function
+ * Return: returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
