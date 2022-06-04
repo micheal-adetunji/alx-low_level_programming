03bd663a72b6bdf6f2dd945811f2a7c738effac3
micheal <mikexpress24@gmail.com>
2022-06-04 00:14:25 +0000

    created 101-quote.c program

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a line of text
+ *
+ * Return: returns 1 on success
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
