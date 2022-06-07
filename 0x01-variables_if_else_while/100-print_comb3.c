8bdddfc0ddc7988e34eb4906cc5b831abdb0fa72
micheal <mikexpress24@gmail.com>
2022-06-07 07:14:26 +0000

    created 100-print_comb3.c program

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - outputs 2 digit numbers
+ *
+ * outputs -outputs 2 digit numbers
+ * Return: returns 0
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 < 9; num1++)
+	{
+		for (num2 = num1 + 1;num2 < 10; num2++)
+		{
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
+			if (num1 == 8 && num2 == 9)
+				continue;
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
