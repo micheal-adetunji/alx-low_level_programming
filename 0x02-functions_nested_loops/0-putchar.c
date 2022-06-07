528d91bb445d0215d4df7ab0587ba49b8bf371da
micheal <mikexpress24@gmail.com>
2022-06-07 16:56:58 +0000

    created _putchar.c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - outputs _putchar string
+ *
+ * outputs _putchar string
+ * Return: returns 0
+ */
+int main(void)
+{
+	_putchar("_putchar");
+	_putchar('\n');
+	return (0);
+}
