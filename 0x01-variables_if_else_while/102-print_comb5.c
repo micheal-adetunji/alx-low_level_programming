1717bf4b185d501e365495685f21c62b671037aa
micheal <mikexpress24@gmail.com>
2022-06-07 08:59:42 +0000

    created 102-print_comb5.c program

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,30 +10,20 @@ int main(void)
 {
 	int num1;
 	int num2;
-	int num3;
-	int num4;
 
-	for (num1 = 0; num1 < 10; num1++)
+	for (num1 = 0; num1 < 99; num1++)
 	{
-		for (num2 = 0; num2 < 9; num2++)
+		for (num2 = num1 + 1; num2 < 100; num2++)
 		{
+			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
-			putchar((num2 % 10) + '0');
-			if (num1 == 9 && num2 == 8)
-				continue;
 			putchar(32);
-			for (num3 = 0; num4 < 10; num3++)
-			{
-				for (num4 = num3 + 1; num4 < 10; num4++)
-				{
-					putchar((num3 % 10) + '0');
-					putchar((num4 % 10) + '0');
-					if (num3 == 9 && num4 == 9)
-						continue;
-					putchar(44);
-					putchar(32);
-				}
-			}
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+			if (num1 == 98 && num2 == 99)
+				continue;
+			putchar(44);
+			putchar(32);
 		}
 	}
 	putchar('\n');
