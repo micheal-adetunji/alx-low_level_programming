cf4b0c45afd9b25921a4dfe84970aa7d82b68ed8
micheal <mikexpress24@gmail.com>
2022-06-08 08:21:47 +0000

    corrected 2-main.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - outputs all lowercase alphabet
+ *
+ * outputs all lowercase alphabet in 10 lines
+ * Return: returns void on success
+ */
+void print_alphabet_x10(void)
+{
+	int x = 1;
+	char ch;
+
+	while (x <= 10)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
+
