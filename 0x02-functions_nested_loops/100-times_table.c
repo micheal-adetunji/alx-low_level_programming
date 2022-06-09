291da72310d8ae966fa051c8ad23946dfb3978e2
micheal <mikexpress24@gmail.com>
2022-06-09 22:08:57 +0000

    created 100-times_table.c program

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * print_times_table - outputs times table of given integer
+ * @n: integer argument
+ *
+ * Return: returns void
+ */
+void print_times_table(int n)
+{
+	int num1, num2, total;
+
+	if ((n >= 0) && (n < 16))
+	{
+		for (num1 = 0; num1 <= n; num1++)
+		{
+			_putchar('0');
+			_putchar(',');
+			_putchar(' ');
+			for (num2 = 1; num2 <= n; num2++)
+			{
+				total = num1 * num2;
+				if ((total / 10) == 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((total % 10) + '0');
+				}
+				else if (((total / 10) > 0) && ((total / 10) <= 9))
+				{
+					_putchar(' ');
+					_putchar((total / 10) + '0');
+					_putchar((total % 10) + '0');
+				}
+				else if (((total / 10) > 9) && ((total / 10) <= 99))
+				{
+					_putchar((total / 100) + '0');
+					_putchar(((total / 10) % 10) + '0');
+					_putchar((total % 10) + '0');
+				}
+				if (num2 < n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
