d11aaf0a8ced15f32bb3173d91f9860c91f3aafe
micheal <mikexpress24@gmail.com>
2022-06-10 01:07:47 +0000

    created 104-fibonacci.c program

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - outputs number
+ *
+ * outputs first 98 fibonacci numbers
+ * starting from 1 and 2
+ * Return: returns 0
+ */
+int main(void)
+{
+	long int num1, num2, next;
+	int n;
+	
+	num1 = 1;
+	num2 = 2;
+	next = num1 + num2;
+	printf("%ld, %ld, ", num1, num2);
+	for (n = 3; n <= 98; n++)
+	{
+		printf("%ld", next);
+		num1 = num2;
+		num2 = next;
+		next = num1 + num2;
+		if (n < 98)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
