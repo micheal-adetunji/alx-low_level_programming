30b83edc52484f9ebe024c716cd6cd95f16fd160
micheal <mikexpress24@gmail.com>
2022-06-14 20:39:10 +0000

    created 0-reset_to_98.c function

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets value of pointing address to 98
+ * @n: pointing address
+ *
+ * Return: return void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
