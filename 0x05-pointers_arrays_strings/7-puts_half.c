3419b15b8feb41499b66cef56e0ea7c978521685
micheal <mikexpress24@gmail.com>
2022-06-22 02:51:59 +0000

    created 7-puts_half.c function

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - outputs 2nd half of string
+ * @str: string argument
+ *
+ * Return: returns void
+ */
+void puts_half(char *str)
+{
+	int i = 0, n = 0;
+
+	while (str[i] != '\0')
+	{
+		n++;
+		i++;
+	}
+	if ((n % 2) == 0)
+	{
+		for (i = n / 2; i < n; ++i)
+			_putchar(str[i]);
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = (n - 1) / 2; i < n; ++i)
+			_putchar(str[i]);
+		_putchar('\n');
+	}
+}
