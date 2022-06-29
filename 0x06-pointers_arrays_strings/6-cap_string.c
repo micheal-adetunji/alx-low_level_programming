55933fd7bbb510f6beea49e1990f3a6997ae6d9a
micheal <mikexpress24@gmail.com>
2022-06-29 19:21:14 +0000

    created 6-cap_string.c function

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,22 +13,25 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i]; i++)
 	{
-		if (str[i - 1] == ' ' ||
-			str[i - 1] == '\t' ||
-			str[i - 1] == '\n' ||
-			str[i - 1] == ',' ||
-			str[i - 1] == ';' ||
-			str[i - 1] == '.' ||
-			str[i - 1] == '!' ||
-			str[i - 1] == '?' ||
-			str[i - 1] == '"' ||
-			str[i - 1] == '(' ||
-			str[i - 1] == ')' ||
-			str[i - 1] == '{' ||
-			str[i - 1] == '}')
-			if (str[i] >= 'a' &&
-				str[i] <= 'z')
-				str[i] = str[i] - 32;
+		if (str[i] == ' ' ||
+			str[i] == '\t' ||
+			str[i] == '\n' ||
+			str[i] == ',' ||
+			str[i] == ';' ||
+			str[i] == '.' ||
+			str[i] == '!' ||
+			str[i] == '?' ||
+			str[i] == '"' ||
+			str[i] == '(' ||
+			str[i] == ')' ||
+			str[i] == '{' ||
+			str[i] == '}')
+			if (str[i + 1] >= 'a' &&
+				str[i + 1] <= 'z')
+			{
+				str[i + 1] = str[i + 1] - 32;
+				continue;
+			}
 	}
 	return (str);
 }
