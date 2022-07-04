490a93c317bcca12d52934e15e7d1efd2f3f1acd
micheal <mikexpress24@gmail.com>
2022-07-04 13:24:39 +0000

    created 4-strpbrk.c function

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any
+ * of a set of bytes
+ * @s: string arg1
+ * @accept: string arg2
+ *
+ * Return: returns pointer to s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+	
+	j = 0;
+	while (s[j])
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (s[j] == accept[i])
+				return (s + j);
+		}
+		j++;
+	}
+	return ('\0');
+}
