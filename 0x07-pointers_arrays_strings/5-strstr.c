77d877334ccf1f33da1cb320a4d479db3e2e9c05
micheal <mikexpress24@gmail.com>
2022-07-05 08:18:23 +0000

    created 5-strstr.c function

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,7 +14,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		char *str = haystack;
 
-		while (*haystack && *needle && *haystack == *needle && *needle != '\0')
+		while (*haystack && *needle && *haystack == *needle && *needle != '\0' && *haystack != '\0')
 		{
 			haystack++;
 			needle++;
