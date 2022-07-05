d731b125899d77ea2e408078de1062d4272c10d8
micheal <mikexpress24@gmail.com>
2022-07-05 07:43:18 +0000

    created 5-strstr.c function

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string argument
+ * @needle: substring to find
+ *
+ * Return: returns pointer to beginning
+ * of located substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		char *str = haystack;
+		while (*haystack && *needle && *haystack == *needle)
+		{
+			haystack++;
+			needle++;
+		}
+		if (!*needle)
+			return (str);
+
+		haystack = str + 1;
+	}
+	return ('\0');
+}
