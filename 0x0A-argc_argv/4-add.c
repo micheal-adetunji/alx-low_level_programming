0af35bf8af6f8818ae0c1c4f86b3a164faaef788
micheal <mikexpress24@gmail.com>
2022-07-13 12:56:19 +0000

    created 4-add.c function

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,14 +10,17 @@
  */
 int main(int argc, char *argv[])
 {
-	int result = 0, i;
+	int result = 0, i, j;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (argv[i][0] < '0' || argv[i][0] > '9')
+		for (j = 0; argv[i][j]; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 		result += atoi(argv[i]);
 	}
